53257f1a6364b908a8c2f1bff34fa15d616a301f
Md Ashraful Alam <anjonbd2007@gmail.com>
2021-03-24 23:39:01 +0100

    install open5gs from ubuntu software repository

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
 
 if ! grep "ogstun" /proc/net/dev > /dev/null; then
     ip tuntap add name ogstun mode tun
@@ -8,3 +9,9 @@ ip addr add 10.45.0.1/16 dev ogstun
 ip addr del 2001:230:cafe::1/48 dev ogstun 2> /dev/null
 ip addr add 2001:230:cafe::1/48 dev ogstun
 ip link set ogstun up
+
+sudo systemctl restart open5gs-mmed
+sudo systemctl restart open5gs-sgwud
+
+child=$!
+wait "$child"
