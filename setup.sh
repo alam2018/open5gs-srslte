65a6425277eaf3f8979b7d9d0b0ccf16a0991b61
Md Ashraful Alam <anjonbd2007@gmail.com>
2021-03-29 18:11:48 +0200

    try

--- setup.sh
+++ setup.sh
@@ -10,7 +10,8 @@ ip addr del 2001:230:cafe::1/48 dev ogstun 2> /dev/null
 ip addr add 2001:230:cafe::1/48 dev ogstun
 ip link set ogstun up
 
-
+#sudo systemctl restart open5gs-mmed
+#sudo systemctl restart open5gs-sgwud
 
 child=$!
 wait "$child"
