563bbdfe3f0bf9a4c6f947797def6e202f8fe3e6
Md Ashraful Alam <anjonbd2007@gmail.com>
2021-04-08 19:52:48 +0200

    try

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ sleep 5
 sleep 5
 ./usr/bin/open5gs-sgwcd /etc/open5gs/sgwc.yaml &
 sleep 5
-./usr/bin/open5gs-pcfd /etc/open5gs/pcf.yaml &
-sleep 5
+#./usr/bin/open5gs-pcfd /etc/open5gs/pcf.yaml &
+#sleep 5
 ./usr/bin/open5gs-pcrfd /etc/open5gs/pcrf.yaml &
 sleep 5
 ./usr/bin/open5gs-hssd /etc/open5gs/hss.yaml &
