9a3d23838bb49dd4a5fe8cc59eb5cc997e9d457d
Md Ashraful Alam <anjonbd2007@gmail.com>
2021-04-08 19:57:19 +0200

    try

--- run.sh
+++ run.sh
@@ -3,19 +3,19 @@
 
 #cp mme.yml /etc/open5gs/
 
-./usr/bin/mongod --config /etc/mongodb.conf &
+/usr/bin/mongod --config /etc/mongodb.conf &
 sleep 5
-./usr/bin/open5gs-mmed /etc/open5gs/mme.yaml &
+/usr/bin/open5gs-mmed /etc/open5gs/mme.yaml &
 sleep 5
-./usr/bin/open5gs-sgwud /etc/open5gs/sgwu.yaml &
+/usr/bin/open5gs-sgwud /etc/open5gs/sgwu.yaml &
 sleep 5
-./usr/bin/open5gs-sgwcd /etc/open5gs/sgwc.yaml &
+/usr/bin/open5gs-sgwcd /etc/open5gs/sgwc.yaml &
 sleep 5
 #./usr/bin/open5gs-pcfd /etc/open5gs/pcf.yaml &
 #sleep 5
-./usr/bin/open5gs-pcrfd /etc/open5gs/pcrf.yaml &
+/usr/bin/open5gs-pcrfd /etc/open5gs/pcrf.yaml &
 sleep 5
-./usr/bin/open5gs-hssd /etc/open5gs/hss.yaml &
+/usr/bin/open5gs-hssd /etc/open5gs/hss.yaml &
 sleep 5
 
 echo "All core VNFs started..."
