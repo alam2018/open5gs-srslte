d3aeebd72187889f94d733ac8cb25c3ef2187ab1
Md Ashraful Alam <anjonbd2007@gmail.com>
2021-04-09 13:18:56 +0200

    try

--- run.sh
+++ run.sh
@@ -18,9 +18,9 @@ echo "Starting SPGWU......."
 /usr/bin/open5gs-sgwud /etc/open5gs/sgwu.yaml &
 sleep 5s
 
-echo "Starting HSS......."
-/usr/bin/open5gs-hssd /etc/open5gs/hss.yaml &
-sleep 5s
+#echo "Starting HSS......."
+#/usr/bin/open5gs-hssd /etc/open5gs/hss.yaml &
+#sleep 5s
 
 #echo "Starting PCF......."
 #./usr/bin/open5gs-pcfd /etc/open5gs/pcf.yaml &
