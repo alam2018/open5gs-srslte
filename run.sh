4633f86bdbb5d96655fa295e01d3e48c9daa5477
Md Ashraful Alam <anjonbd2007@gmail.com>
2021-04-09 13:10:47 +0200

    try

--- run.sh
+++ run.sh
@@ -8,15 +8,15 @@ sleep 5s
 /usr/bin/open5gs-mmed /etc/open5gs/mme.yaml &
 sleep 5s
 /usr/bin/open5gs-sgwcd /etc/open5gs/sgwc.yaml &
-sleep 5
+sleep 5s
 /usr/bin/open5gs-sgwud /etc/open5gs/sgwu.yaml &
-sleep 5
+sleep 5s
 /usr/bin/open5gs-hssd /etc/open5gs/hss.yaml &
-sleep 5
+sleep 5s
 ./usr/bin/open5gs-pcfd /etc/open5gs/pcf.yaml &
-sleep 5
+sleep 5s
 /usr/bin/open5gs-pcrfd /etc/open5gs/pcrf.yaml &
-sleep 5
+sleep 5s
 
 
 echo "All core VNFs started..."
