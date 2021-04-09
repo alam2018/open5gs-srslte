704cca0d0be89afee5e0d8b47bac1eb7b87a78b6
Md Ashraful Alam <anjonbd2007@gmail.com>
2021-04-09 14:06:20 +0200

    try

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
 
-if ! grep "ogstun" /proc/net/dev > /dev/null; then
-    ip tuntap add name ogstun mode tun
-fi
+ip tuntap add name ogstun mode tun
 ip addr del 10.45.0.1/16 dev ogstun 2> /dev/null
 ip addr add 10.45.0.1/16 dev ogstun
 ip addr del 2001:230:cafe::1/48 dev ogstun 2> /dev/null
