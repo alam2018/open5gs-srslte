22922e7b60a4d5d2ad4191177a7384fa9322aa91
Md Ashraful Alam <anjonbd2007@gmail.com>
2021-04-09 13:17:22 +0200

    try

--- run.sh
+++ run.sh
@@ -22,9 +22,9 @@ echo "Starting HSS......."
 /usr/bin/open5gs-hssd /etc/open5gs/hss.yaml &
 sleep 5s
 
-echo "Starting PCF......."
-./usr/bin/open5gs-pcfd /etc/open5gs/pcf.yaml &
-sleep 5s
+#echo "Starting PCF......."
+#./usr/bin/open5gs-pcfd /etc/open5gs/pcf.yaml &
+#sleep 5s
 
 echo "Starting PCRF......."
 /usr/bin/open5gs-pcrfd /etc/open5gs/pcrf.yaml &
