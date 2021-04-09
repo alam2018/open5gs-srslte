f2ac7ac3b9ccd844500e9533ef5e31b0dd660508
Md Ashraful Alam <anjonbd2007@gmail.com>
2021-04-09 12:11:40 +0200

    try

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 #cp mme.yml /etc/open5gs/
 
 /usr/bin/mongod --config /etc/mongodb.conf &
-sleep 5
-/usr/bin/open5gs-mmed /etc/open5gs/mme.yaml &
+#sleep 5
+/usr/bin/open5gs-mmed /etc/open5gs/mme.yaml 
 #sleep 5
 #/usr/bin/open5gs-sgwcd /etc/open5gs/sgwc.yaml &
 #sleep 5
