6ee751419dc13f2903345c8b79c19496361b19f7
Md Ashraful Alam <anjonbd2007@gmail.com>
2021-04-09 16:18:07 +0200

    try

--- deploy-test.sh
+++ deploy-test.sh
@@ -7,23 +7,23 @@ mongod --config /etc/mongodb.conf &
 sleep 5s
 
 echo "Starting PCRF......."
-/usr/bin/open5gs-pcrfd /etc/open5gs/pcrf.yaml &
+/bin/open5gs-pcrfd /etc/open5gs/pcrf.yaml &
 sleep 5s
 
 echo "Starting MME......."
-/usr/bin/open5gs-mmed /etc/open5gs/mme.yaml &
+/bin/open5gs-mmed /etc/open5gs/mme.yaml &
 sleep 5s
 
 echo "Starting SPGWU......."
-/usr/bin/open5gs-sgwud /etc/open5gs/sgwu.yaml &
+/bin/open5gs-sgwud /etc/open5gs/sgwu.yaml &
 sleep 5s
 
 echo "Starting SPGWC......."
-/usr/bin/open5gs-sgwcd /etc/open5gs/sgwc.yaml &
+/bin/open5gs-sgwcd /etc/open5gs/sgwc.yaml &
 sleep 5s
 
 echo "Starting HSS......."
-/usr/bin/open5gs-hssd /etc/open5gs/hss.yaml &
+/bin/open5gs-hssd /etc/open5gs/hss.yaml &
 sleep 5s
 
 #echo "Starting PCF......."
